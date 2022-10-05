f56e380b03b1f5ffadafa767430cb328d9068501
moreiraConcepta <rmoreira@conceptainvest.com>
2022-10-05 19:51:52 -0300

    adds multiple scenes

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -11,11 +12,31 @@ public class CollisionHandler : MonoBehaviour
                 UnityEngine.Debug.Log("This is friendly");
                 break;
             case "Finish":
-                UnityEngine.Debug.Log("This is Finish");
+                NextLevel();
                 break;    
             case "Fuel":
                 UnityEngine.Debug.Log("This is Fuel");
-                break;       
+                break; 
+            default:
+                ReloadLevel();
+                break;          
         }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = ++currentSceneIndex;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
